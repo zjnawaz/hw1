9cee26d271fa9c0ca2671394bf4e78146b42f4a4
Zainab Nawaz <znawaz@usc.edu>
2023-01-19 05:58:58 +0000

    Initial commit for hw1

---
+++ grade_split.sh
@@ -0,0 +1,49 @@
+g++ split.cpp grade_split.cpp -o grade_split `pkg-config --cflags --libs gtest`
+TOTAL=12
+retVal=$?
+    if [ $retVal -eq 0 ]
+    then
+        echo "grade_split compiled."
+    else
+		echo "grade_split did not compile. Make sure your split.cpp compiles with your test code."
+		echo "AUTO:T$TOTAL:P0:V0:0.0"
+		return -1
+    fi
+PASSED=0
+FAILED=0
+FAILED_TESTS=""
+for test in Split.Empty Split.InputSetToNull Split.OneEven Split.OneOdd Split.TwoNumbers Split.Zero Split.AllOddsOneEven Split.TenNumbers Split.AllEvens Split.AllOdds Split.Duplicates Split.DuplicatesMixed
+do
+    echo $test
+    ./grade_split --gtest_filter=$test
+    retVal=$?
+    if [ $retVal -eq 0 ]
+    then
+        let PASSED=PASSED+1
+    else
+		let FAILED=FAILED+1
+		FAILED_TESTS="${FAILED_TESTS} "$test
+    fi
+done
+VPASSED=0
+VFAILED=0
+VFAILED_TESTS=""
+for test in Split.Empty Split.InputSetToNull Split.OneEven Split.OneOdd Split.TwoNumbers Split.Zero Split.AllOddsOneEven Split.TenNumbers Split.AllEvens Split.AllOdds Split.Duplicates Split.DuplicatesMixed
+do
+    echo $test
+    valgrind --tool=memcheck --leak-check=yes ./grade_split --gtest_filter=$test
+    retVal=$?
+    if [ $retVal -eq 0 ]
+    then
+        let VPASSED=VPASSED+1
+    else
+		let VFAILED=VFAILED+1
+		VFAILED_TESTS="${VFAILED_TESTS} "$test
+    fi
+done
+echo "The following tests failed: ${FAILED_TESTS}"
+echo "The following tests failed valgrind: ${VFAILED_TESTS}"
+S="max(100*($PASSED/$TOTAL - 0.5*($TOTAL-$VPASSED)/$TOTAL),0)"
+SCORE=`python -c "print(round($S,2))"`
+echo "Score = $S = $SCORE"
+echo "AUTO:T$TOTAL:P$PASSED:V$VPASSED:$SCORE"
